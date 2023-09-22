acc3cad0f9ad0cfd1b86db990fcb656bb46bb8f7
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2023-09-22 12:14:22 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListOfOrganizations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int a1 = 1;
+            string connectsql = "";
+            SqlConnection connection = new SqlConnection(connectsql);
+            connection.Open();
+            string query = "Select * From list";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            int label1location = label1.Bottom;
+            while (reader.Read())
+            {
+                Label l1 = new Label();
+                l1.Top = label1location + 30 * a1;
+                l1.Left = this.Left + 10;
+                l1.Text = reader.GetValue(1).ToString();
+                l1.TextAlign = ContentAlignment.MiddleCenter;
+                Label l2 = new Label();
+                l2.Top = label1location + 30 * a1;
+                l2.Left = l1.Right + 10;
+                l2.Text = reader.GetValue(2).ToString();
+                l2.TextAlign = ContentAlignment.MiddleLeft;
+                Label l3 = new Label();
+                l3.Top = label1location + 30 * a1;
+                l3.Left = l2.Right + 10;
+                l3.Text = reader.GetValue(3).ToString();
+                l3.TextAlign = ContentAlignment.MiddleCenter;
+
+                this.Controls.Add(l1);
+                this.Controls.Add(l2);
+                this.Controls.Add(l3);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 formtoopen=new Form2();
+            formtoopen.ShowDialog();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListOfOrganizations
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") { 
+                string text1 = textBox1.Text;
+                string text2 = textBox2.Text;
+                string text3=textBox3.Text;
+
+                string connectsql = "";
+                SqlConnection connectionn = new SqlConnection(connectsql);
+                connectionn.Open();
+                string query = "INSERT INTO list (nazwa, osoba, stan) VALUES (@textt1, @textt2, @textt3)";
+                SqlCommand commm = new SqlCommand(query, connectionn);
+                commm.Parameters.AddWithValue("@textt1", text1);
+                commm.Parameters.AddWithValue("@textt2", text2);
+                commm.Parameters.AddWithValue("@textt3", text3);
+                commm.ExecuteNonQuery();
+                connectionn.Close();
+            }
+            else
+            {
+                MessageBox.Show("Write what you want to add in first field");
+            }
+
+        }
+    }
+}
